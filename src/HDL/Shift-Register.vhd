15f016b3330ee9e7fb72fce24ea3dc7466bf8072
Isaiah Mize <idcmize@gmail.com>
2026-02-25 20:52:55 -0700

    Added a simple shift register however needs more testing.

--- src/HDL/Shift-Register.vhd
+++ src/HDL/Shift-Register.vhd
@@ -36,14 +36,28 @@ entity ShiftRegister is
     Port ( 
     clk: in std_logic;
     reset: in std_logic;
-    input_bits: in std_logic_vector(8 downto 0);
-    output_bits: out std_logic_vector(8 downto 0)
+    input_bit: in std_logic;
+    output_bits: out std_logic_vector(7 downto 0)
     );
 end ShiftRegister;
 
 architecture Behavioral of ShiftRegister is
 
+signal bits_reg: std_logic_vector(7 downto 0);
+signal bits_next: std_logic_vector(7 downto 0);
 
 begin
+process(clk, reset)
+begin    
+    if(reset='1') then
+        bits_reg <= (others => '0');
+    elsif(clk'event and clk='1') then
+        bits_reg <= bits_next;
+    end if;
+end process;
+
+bits_next <= bits_reg(6 downto 0) & input_bit;
+
+output_bits <= bits_reg;
 
 end Behavioral;
---
+++ test-bench/ShiftReg_TB.sv
@@ -0,0 +1,73 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 02/25/2026 08:06:40 PM
+// Design Name: 
+// Module Name: ShiftReg_TB
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module ShiftReg_TB();
+
+reg reset;
+reg d;
+reg [7:0] output_bits;
+reg clk;
+
+initial
+begin
+
+#10 clk <= 1;
+d <= 1;
+#10 clk <= 0;
+
+#10 clk <= 1;
+#10 clk <= 0;
+d <= 1;
+
+#10 clk <= 1;
+#10 clk <= 0;
+d <= 1;
+
+#10 clk <= 1;
+#10 clk <= 0;
+d <= 1;
+
+#10 clk <= 1;
+#10 clk <= 0;
+d <= 1;
+
+#10 clk <= 1;
+#10 clk <= 0;
+d <= 1;
+
+#10 clk <= 1;
+#10 clk <= 0;
+d <= 1;
+
+#10 clk <= 1;
+#10 clk <= 0;
+d <= 1;
+
+end
+
+ShiftRegister DUT (.clk (clk),
+                   .reset (reset),
+                   .input_bit (d),
+                   .output_bits (output_bits)
+                   );
+
+endmodule
